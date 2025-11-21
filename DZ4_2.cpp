be318368d91e82e43b58aa9765669df367a3aee6
ROMANGREKHOV <94071555+ROMANGREKHOV@users.noreply.github.com>
2025-11-21 23:10:42 +0300

    Update DZ4_2.cpp

--- DZ4_2.cpp
+++ DZ4_2.cpp
@@ -1,17 +1,18 @@
-//#include <iostream>
-//
-//int main() {
-//	int var;
-//	std::cout << "Введите целое число: " << std::endl;
-//	std::cin >> var;
-//
-//	int otv{ 0 };
-//	while (var != 0) {
-//		otv += var % 10;
-//		var = var / 10;
-//	};
-//
-//	std::cout << "Сумма цифр: " << otv;
-//
-//	return EXIT_SUCCESS;
-//}
+#include <iostream>
+
+int main() {
+	int var;
+	std::cout << "Введите целое число: " << std::endl;
+	std::cin >> var;
+
+	int otv{ 0 };
+	while (var != 0) {
+		otv += var % 10;
+		var = var / 10;
+	};
+
+	std::cout << "Сумма цифр: " << otv;
+
+	return EXIT_SUCCESS;
+
+}
