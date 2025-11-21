7db290df9113c2ebd478595826e1259921e67a31
ROMANGREKHOV <94071555+ROMANGREKHOV@users.noreply.github.com>
2025-11-21 23:09:39 +0300

    Add files via upload

---
+++ DZ4_1.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//
+//int main() {
+//
+//	int var1,var2;
+//	var2 = 0;
+//	do {
+//		std::cout << "Введите целое число или число '0', чтобы закончить: " << std::endl;
+//		std::cin >> var1;
+//		var2 += var1;
+//
+//	} while (var1 != 0);
+//
+//	std::cout << "Сумма: " << var2;
+//
+//	return EXIT_SUCCESS;
+//}
---
+++ DZ4_3.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//
+//int main() {
+//	int var;
+//	std::cout << "Введите целое число: " << std::endl;
+//	std::cin >> var;
+//
+//	for (int i = 1; i <= 10; ++i) {
+//		std::cout << var << " X " << i << " = " << var * i << std::endl;
+//	}
+//
+//	return EXIT_SUCCESS;
+//}
