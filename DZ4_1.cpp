15c5f7c4c286b1bf3ca0139214fc876a5fb9bb1c
ROMANGREKHOV <94071555+ROMANGREKHOV@users.noreply.github.com>
2025-11-21 23:09:56 +0300

    DZ4_1.cpp

--- DZ4_1.cpp
+++ DZ4_1.cpp
@@ -1,17 +1,18 @@
-//#include <iostream>
-//
-//int main() {
-//
-//	int var1,var2;
-//	var2 = 0;
-//	do {
-//		std::cout << "Введите целое число или число '0', чтобы закончить: " << std::endl;
-//		std::cin >> var1;
-//		var2 += var1;
-//
-//	} while (var1 != 0);
-//
-//	std::cout << "Сумма: " << var2;
-//
-//	return EXIT_SUCCESS;
-//}
+#include <iostream>
+
+int main() {
+
+	int var1,var2;
+	var2 = 0;
+	do {
+		std::cout << "Введите целое число или число '0', чтобы закончить: " << std::endl;
+		std::cin >> var1;
+		var2 += var1;
+
+	} while (var1 != 0);
+
+	std::cout << "Сумма: " << var2;
+
+	return EXIT_SUCCESS;
+
+}
